7035e8c9d34ca8f50559d8af06cb233f0b1ba689
ivan1810 <78267912+IvanRobak@users.noreply.github.com>
2023-12-26 21:12:09 +0200

    status

--- src/components/ContentInfo/ContentInfo.jsx
+++ src/components/ContentInfo/ContentInfo.jsx
@@ -2,51 +2,53 @@ import { Component } from 'react';
 import getNews from '../../services/getNews';
 import ErrorCard from '../ErrorCard/ErrorCard';
 
+const STATUS = {
+  IDLE: 'idle',
+  PENDING: 'pending',
+  REJECTED: 'rejected',
+  RESOLVED: 'resolved',
+};
 class ContentInfo extends Component {
   state = {
     news: null,
-    isLoading: true,
     error: '',
+    status: STATUS.IDLE,
   };
 
   componentDidUpdate(prevProps, prevstate) {
-    console.log(this.props);
     if (prevProps.searchText !== this.props.searchText) {
-      this.setState({ isLoading: true });
+      this.setState({ status: STATUS.PENDING });
       getNews(this.props.searchText)
         .then(response => response.json())
         .then(data => {
-          if (data.status === 'ok') this.setState({ news: data.articles });
+          if (data.status === 'ok') this.setState({ news: data.articles, status: STATUS.RESOLVED });
           else return Promise.reject(data.message);
         })
         .catch(error => {
-          this.setState({ error });
-        })
-        .finally(() => {
-          this.setState({ isLoading: false });
+          this.setState({ error, status: STATUS.REJECTED });
         });
     }
   }
 
   render() {
-    const { news, isLoading, error } = this.state;
+    const { news, error, status } = this.state;
 
-    return (
-      <>
-        {error && <ErrorCard>{this.state.error}</ErrorCard>}
-        {isLoading && (
-          <div class="spinner-border" role="status">
-            <span class="visually-hidden">Loading...</span>
-          </div>
-        )}
+    if (status === STATUS.PENDING)
+      return (
+        <div className="spinner-border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      );
+    else if (status === STATUS.REJECTED) return <ErrorCard>{error}</ErrorCard>;
+    else if (status === STATUS.RESOLVED)
+      return (
         <ul>
           {news &&
             news.map(el => {
               return <li key={el.url}>{el.title}</li>;
             })}
         </ul>
-      </>
-    );
+      );
   }
 }
 export default ContentInfo;
